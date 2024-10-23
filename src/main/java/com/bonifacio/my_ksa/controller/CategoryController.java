6c0b036af5fc3650e5aa0b21e3642b18c003dfb9
Bonifacio Juarez Ceja <bonijuarezceja96@outlook.com>
2024-10-23 12:12:33 -0600

    add bulma framework and creation nav and index view

--- src/main/java/com/bonifacio/my_ksa/controller/CategoryController.java
+++ src/main/java/com/bonifacio/my_ksa/controller/CategoryController.java
@@ -3,6 +3,8 @@ package com.bonifacio.my_ksa.controller;
 import com.bonifacio.my_ksa.services.category.ICategoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +16,13 @@ public class CategoryController {
     @Autowired
     private final ICategoryService categoryService;
 
-    @RequestMapping(value = "/")
-    public String Index(Model model){
+    @RequestMapping(value = {"/",""})
+    public String Index(
+            Model model,
+            @PageableDefault(page = 0,size = 10) Pageable pageable){
+        var categories = categoryService.getAll(pageable);
         model.addAttribute("title","Categorias");
+        model.addAttribute("categories",categories);
         return "category/index";
     }
 }
